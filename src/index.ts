86bf747bc9d22728abdf440c2323d4887ef84145
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-08-25 20:29:46 +0700

    add morgan to log request

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 import routes from './routes';
 
 // Load enviroment variables
 dotenv.config({ path: './config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 app.use('/api/v1', routes);
 
